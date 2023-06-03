49420d96033e7f65c0da6cef30decf2aadd08178
Ejun Park <jjoonleo@gmail.com>
2023-06-03 19:58:53 +0900

    delete dioclient provider

--- lib/core/api/module.dart
+++ lib/core/api/module.dart
@@ -1,8 +1,4 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter_library_app/core/core.dart';
-import 'package:flutter_library_app/feature/user/presentation/viewmodel/module.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final dioClientProvider = Provider<DioClient>((ref) {
-  return DioClient(null);
-});
+// final dioClientProvider = Provider<DioClient>((ref) {
+//   return DioClient(null);
+// });
