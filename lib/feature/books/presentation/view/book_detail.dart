9558ed35dfdcafbea46d9bc685244b37e7df9ffa
Ejun Park <jjoonleo@gmail.com>
2023-06-04 18:13:36 +0900

    add button disable and scrollview and hero to book detail page

--- lib/feature/books/presentation/view/book_detail.dart
+++ lib/feature/books/presentation/view/book_detail.dart
@@ -1,3 +1,4 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_library_app/core/core.dart';
 import 'package:flutter_library_app/feature/books/books.dart';
@@ -15,6 +16,12 @@ class _BookDetailState extends ConsumerState<BookDetail> {
   late final booksList = ref.read(booksListModel);
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final fullHeight = MediaQuery.of(context).size.height;
+    final appBar = AppBar(); //Need to instantiate this here to get its size
+    final appBarHeight =
+        appBar.preferredSize.height + MediaQuery.of(context).padding.top;
+    final scaffoldBodyHeight = fullHeight - appBarHeight;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.book.title),
@@ -24,21 +31,43 @@ class _BookDetailState extends ConsumerState<BookDetail> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              Image.network(
-                APIList.baseUrl + widget.book.img,
-                width: 300,
-              ),
               SizedBox(
-                height: 20,
-              ),
-              Text(widget.book.description ?? ""),
-              SizedBox(
-                height: 50,
+                height: scaffoldBodyHeight - 90,
+                child: SingleChildScrollView(
+                  child: Column(children: [
+                    Hero(
+                      tag: widget.book.id,
+                      child: Image.network(
+                        APIList.baseUrl + widget.book.img,
+                        width: MediaQuery.of(context).size.height / 3,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(widget.book.description ?? ""),
+                    SizedBox(
+                      height: 50,
+                    ),
+                  ]),
+                ),
               ),
               FilledButton(
-                  onPressed: () {
-                    booksList.borrow(widget.book);
-                  },
+                  onPressed: widget.book.available != null
+                      ? null
+                      : () async {
+                          Either<String, Book> book =
+                              await booksList.borrow(widget.book);
+                          book.fold((l) => debugPrint(l),
+                              (r) => debugPrint(r.toString()));
+                        },
+                  style: ButtonStyle(
+                    backgroundColor: widget.book.available != null
+                        ? MaterialStateProperty.all<Color>(
+                            theme.colorScheme.primaryContainer)
+                        : MaterialStateProperty.all<Color>(
+                            theme.colorScheme.primary),
+                  ),
                   child: SizedBox(
                       width: double.maxFinite,
                       height: 60,
