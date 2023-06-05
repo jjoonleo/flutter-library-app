c49f2f0e8d10a088bafc68333789b6d901a70fd0
Ejun Park <jjoonleo@gmail.com>
2023-06-05 16:13:24 +0900

    use Booktile on books_list and add profile button to appbar

--- lib/feature/books/presentation/view/books_list.dart
+++ lib/feature/books/presentation/view/books_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_library_app/core/core.dart';
 import 'package:flutter_library_app/feature/books/presentation/widgets/book_tile.dart';
 import 'package:flutter_library_app/feature/user/presentation/viewmodel/module.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,7 +8,6 @@ import 'package:flutter_library_app/feature/books/books.dart';
 
 class BooksList extends ConsumerStatefulWidget {
   const BooksList({super.key});
-  
 
   @override
   ConsumerState<BooksList> createState() => _BooksListState();
@@ -15,62 +15,73 @@ class BooksList extends ConsumerStatefulWidget {
 
 class _BooksListState extends ConsumerState<BooksList> {
   late final booksModel = ref.read(booksListModel);
-  late final user = ref.watch(userState);
 
   Future<void> _reloadBooks() async {
     await booksModel.loadBooks();
   }
 
-
   @override
   Widget build(BuildContext context) {
     final booksState = ref.watch(booksListState);
+    final user = ref.watch(userState);
+    final fullHeight = MediaQuery.of(context).size.height;
+    final appBar = AppBar(); //Need to instantiate this here to get its size
+    final appBarHeight =
+        appBar.preferredSize.height + MediaQuery.of(context).padding.top;
+    final scaffoldBodyHeight = fullHeight - appBarHeight;
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        title: const Text("BooksList"),
+        title: Center(
+            child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+          SizedBox(
+            width: 32.0,
+          ),
+          Expanded(child: Center(child: Text("도서목록"))),
+          SizedBox(
+              width: 32.0,
+              height: 32.0,
+              child: IconButton.outlined(
+                  onPressed: () {
+                    user.when(notLoggedIn: () {
+                      context.push("/users/login");
+                    }, loggedIn: (data) {
+                      context.push("/users");
+                    }, error: (data) {
+                      Modal.build(
+                          "에러", "알 수 없는 에러가 발생하였습니다. 관리자에게 문의해 주세요", context);
+                    });
+                  },
+                  icon: Icon(
+                    Icons.person,
+                    size: 16.0,
+                  )))
+        ])),
       ),
       body: Column(children: [
         booksState.when(data: (books) {
           return Column(
             children: [
               SizedBox(
-                height: MediaQuery.of(context).size.height - 300,
+                height: scaffoldBodyHeight,
                 child: RefreshIndicator(
                   onRefresh: _reloadBooks,
                   child: ListView.builder(
                     itemCount: books.values.length,
                     itemBuilder: (itemcontext, index) {
                       final book = books.values[index];
-                      return BookTile(book: book);
+                      return BookTile(id: book.id);
                     },
                   ),
                 ),
               ),
-              ElevatedButton(
-                  onPressed: () {
-                    user.when(
-                        notLoggedIn: () {},
-                        loggedIn: (data) async {
-                          var result = await booksModel.borrow(books.values[0]);
-                          result.fold((l) => debugPrint(l),
-                              (r) => debugPrint(r.toString()));
-                        },
-                        error: (msg) {});
-                  },
-                  child: const Text("borrow")),
             ],
           );
         }, error: (String? message) {
           return const Text("error");
         }, loading: () {
-          return const CircularProgressIndicator();
+          return Container();
         }),
-        ElevatedButton(
-            onPressed: () async {
-              await booksModel.loadBooks();
-            },
-            child: const Text("reload")),
       ]),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
